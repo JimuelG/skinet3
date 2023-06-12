741d2dbb2e0c4547e99e439d81d656237a45b765
Jimuel14 <69880391+Jimuel14@users.noreply.github.com>
2023-06-12 17:04:38 +0800

    End of section 3

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _repo;
+        public ProductsController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return Ok(await _repo.GetProductsAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _repo.GetProductByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrands()
+        {
+            return await _repo.GetProductBrandsAsync();
+        }
+
+        [HttpGet("types")]
+        public async Task<IReadOnlyList<ProductType>> GetProductTypes()
+        {
+            return await _repo.GetProductTypesAsync();
+        }
+    }
+}
